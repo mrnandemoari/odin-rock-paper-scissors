bdd29b75b3e88a5f7247af5168ec14dc1231ee71
mrnandemoari <mrnandemoari@gmail.com>
2024-02-01 16:31:39 +0700

    Create UI for the game, add extra logic to the script

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function getComputerChoice() {
 
 let playerScore = 0;
 let computerScore = 0;
+let roundsPlayed = 0;
 
 function playRound(playerSelection, computerSelection) {
   psLowerCase = playerSelection.toLowerCase();
@@ -46,37 +47,76 @@ function playRound(playerSelection, computerSelection) {
 
 console.log('Rock-Paper-Scissors Start!');
 
-function playGame() {
-  const roundsPerGame = 5;
-  for (let index = 1; index <= roundsPerGame; index++) {
-    let playerSelection = prompt(
-      'Choose your sign: (Rock, Paper, or Scissors)'
-    ).toLowerCase();
-    let computerSelection = getComputerChoice();
+function playGame(playerSelection) {
+  const computerSelection = getComputerChoice();
+  const result = playRound(playerSelection, computerSelection);
 
-    if (
-      playerSelection != 'rock' &&
-      playerSelection != 'paper' &&
-      playerSelection != 'scissors'
-    ) {
-      alert('Invalid choice. Please choose Rock, Paper, or Scissors');
-      index--;
-      continue;
-    }
+  displayResult(result);
+  updateScores();
 
-    console.log("You've chosen " + playerSelection);
-    console.log('Computer has chosen ' + computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-    console.log(
-      'player: ' + playerScore + ' | ' + 'computer: ' + computerScore
-    );
-  }
+  roundsPlayed++;
 
-  if (playerScore > computerScore) {
-    console.log('You win the game!');
-  } else {
-    console.log("Don't worry. Try better next time.");
+  if (roundsPlayed === 5) {
+    determineWinner();
   }
 }
 
-playGame();
+function determineWinner() {
+  if (playerScore > computerScore) {
+    displayResult('You win the game! Congratulations!');
+  } else if (playerScore < computerScore) {
+    displayResult('Computer wins the game. Better luck next time!');
+  } else {
+    displayResult("It's a tie! The game ends in a draw.");
+  }
+
+  // Reset scores and roundsPlayed for a new game
+  playerScore = 0;
+  computerScore = 0;
+  roundsPlayed = 0;
+}
+
+function displayResult(result) {
+  const resultDiv = document.getElementById('result');
+  resultDiv.textContent = result;
+}
+
+function updateScores() {
+  document.getElementById('playerScore').textContent = playerScore;
+  document.getElementById('computerScore').textContent = computerScore;
+}
+
+// Your existing functions getComputerChoice() and playRound() go here
+
+// function playGame() {
+//   const roundsPerGame = 5;
+//   for (let index = 1; index <= roundsPerGame; index++) {
+//     let playerSelection = prompt(
+//       'Choose your sign: (Rock, Paper, or Scissors)'
+//     ).toLowerCase();
+//     let computerSelection = getComputerChoice();
+
+//     if (
+//       playerSelection != 'rock' &&
+//       playerSelection != 'paper' &&
+//       playerSelection != 'scissors'
+//     ) {
+//       alert('Invalid choice. Please choose Rock, Paper, or Scissors');
+//       index--;
+//       continue;
+//     }
+
+//     console.log("You've chosen " + playerSelection);
+//     console.log('Computer has chosen ' + computerSelection);
+//     console.log(playRound(playerSelection, computerSelection));
+//     console.log(
+//       'player: ' + playerScore + ' | ' + 'computer: ' + computerScore
+//     );
+//   }
+
+//   if (playerScore > computerScore) {
+//     console.log('You win the game!');
+//   } else {
+//     console.log("Don't worry. Try better next time.");
+//   }
+// }
